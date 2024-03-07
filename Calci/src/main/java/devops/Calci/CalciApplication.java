481182c36f2468df4ab1ff34e2b90ea606466b62
L1 <srikar@srikar.myguest.virtualbox.org>
2024-03-07 12:03:09 +0530

    Added the advanced Calci

--- Calci/src/main/java/devops/Calci/CalciApplication.java
+++ Calci/src/main/java/devops/Calci/CalciApplication.java
@@ -11,16 +11,55 @@ import java.util.Scanner;
 public class CalciApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalciApplication.class, args);
-	  double num1,num2,Result;
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter the numbers ");
-        num1=sc.nextDouble();
-        num2=sc.nextDouble();
-        Result=num1+num2;
-        System.out.print("Addition of two numbers " + Result);
-      
-	
-	}
+    char operator;
+    Double number1, number2, result;
 
-}
+    // create an object of Scanner class
+    Scanner input = new Scanner(System.in);
+
+    // ask users to enter operator
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
+
+    // ask users to enter numbers
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {
+
+      // performs addition between numbers
+      case '+':
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
+
+      // performs subtraction between numbers
+      case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      // performs multiplication between numbers
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}	
+	
